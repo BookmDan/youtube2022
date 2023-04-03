48fb736fd6b4e0ad79530d72285a97eab6cbc92e
BookmDan <danoh329@gmail.com>
2023-04-02 17:42:12 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import ninjaProfile from "../img/ninja.png";
+// import {signOut} from "firebase/auth"
+// import { auth } from '../firebase'
+// import { AuthContext } from '../context/AuthContext'
+
+const Navbar = () => {
+  // const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className='navbar'>
+      <span className="logo">PeopleWelcome | Chats</span>
+      <div className="user">
+        <img src={ninjaProfile} alt="" />
+        <span> Daniel</span>
+        <button>logout</button>
+        {/* <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button> */}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
