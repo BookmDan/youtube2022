c3606c81836d1791f8c1261ec99cbb19010cd955
BookmDan <danoh329@gmail.com>
2023-04-04 12:36:25 -0700

    search function for username -> storing userChats

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,115 +1,106 @@
 import React, { useContext, useState } from "react";
 import search from "../img/search.png";
-// import {
-//   collection,
-//   query,
-//   where,
-//   getDocs,
-//   setDoc,
-//   doc,
-//   updateDoc,
-//   serverTimestamp,
-//   getDoc,
-// } from "firebase/firestore";
-// import { db } from "../firebase";
-// import { AuthContext } from "../context/AuthContext";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+
 const Search = () => {
-  // const [username, setUsername] = useState("");
-  // const [user, setUser] = useState(null);
-  // const [err, setErr] = useState(false);
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
-  // // const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
-  // const handleSearch = async () => {
-  //   const q = query(
-  //     collection(db, "users"),
-  //     where("displayName", "==", username)
-  //   );
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
 
-  //   try {
-  //     const querySnapshot = await getDocs(q);
-  //     querySnapshot.forEach((doc) => {
-  //       setUser(doc.data());
-  //     });
-  //   } catch (err) {
-  //     setErr(true);
-  //   }
-  // };
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
 
-  // const handleKey = (e) => {
-  //   e.code === "Enter" && handleSearch();
-  // };
+  const handleSelect = async () => {
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
 
-  // const handleSelect = async () => {
-  //   //check whether the group(chats in firestore) exists, if not create
-  //   const combinedId =
-  //     currentUser.uid > user.uid
-  //       ? currentUser.uid + user.uid
-  //       : user.uid + currentUser.uid;
-  //   try {
-  //     const res = await getDoc(doc(db, "chats", combinedId));
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-  //     if (!res.exists()) {
-  //       //create a chat in chats collection
-  //       await setDoc(doc(db, "chats", combinedId), { messages: [] });
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
 
-  //       //create user chats
-  //       await updateDoc(doc(db, "userChats", currentUser.uid), {
-  //         [combinedId + ".userInfo"]: {
-  //           uid: user.uid,
-  //           displayName: user.displayName,
-  //           photoURL: user.photoURL,
-  //         },
-  //         [combinedId + ".date"]: serverTimestamp(),
-  //       });
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
 
-  //       await updateDoc(doc(db, "userChats", user.uid), {
-  //         [combinedId + ".userInfo"]: {
-  //           uid: currentUser.uid,
-  //           displayName: currentUser.displayName,
-  //           photoURL: currentUser.photoURL,
-  //         },
-  //         [combinedId + ".date"]: serverTimestamp(),
-  //       });
-  //     }
-  //   } catch (err) {}
-
-  //   setUser(null);
-  //   setUsername("")
-  // };
+    setUser(null);
+    setUsername("")
+  };
   return (
     <div className="search">
       <div className="searchForm">
         <div className="searchIcon">
           <img src={search} alt="" />
           <input
-          type="text"
-          placeholder="Find a user"
-          // onKeyDown={handleKey}
-          // onChange={(e) => setUsername(e.target.value)}
-          // value={username}
-        />
-        </div>
-        <div className="userChat">
-          <img src="https://media.licdn.com/dms/image/C5603AQF6C2Ua_S1hCQ/profile-displayphoto-shrink_200_200/0/1652992176979?e=1686182400&v=beta&t=yYxhuaJbAnbUYhbLedzDPLnuIMzCrCPe-2qt8MoggwM" alt="" />
-      
-          <div className="userChatInfo">
-            <span>Jane</span>
-  
-          </div>
+            type="text"
+            placeholder="Find a user"
+            onKeyDown={handleKey}
+            onChange={(e) => setUsername(e.target.value)}
+            value={username} 
+          />
         </div>
       </div>
-      {/* {err && <span>User not found!</span>}
+      {err && <span>User not found!</span>}
       {user && (
         <div className="userChat" onClick={handleSelect}>
-          <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/dde4f8d1-beeb-4575-87e9-471821d4fed3-profile_image-70x70.png" alt="" />
           <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
-            <span>Jane</span>
             <span>{user.displayName}</span>
           </div>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
